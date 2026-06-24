5d219746f5182cf0396901fe6461fe5a518ab7b5
zephyrq-z <zephyrqz@163.com>
2026-06-24 11:28:06 +0800

    init

--- paper_spiders/utils/paperlist.py
+++ paper_spiders/utils/paperlist.py
@@ -1,66 +1,4 @@
 paper_list = [
-    # 2020
-    {
-        "conf": "ICSE 2020",
-        "url": "https://2020.icse-conferences.org/track/icse-2020-papers",
-    },
-    {"conf": "FSE 2020", "url": "https://2020.esec-fse.org/track/fse-2020-papers"},
-    {
-        "conf": "ASE 2020",
-        "url": "https://conf.researchr.org/track/ase-2020/ase-2020-papers",
-    },
-    {
-        "conf": "ISSTA 2020",
-        "url": "https://conf.researchr.org/track/issta-2020/issta-2020-papers",
-    },
-    # 2021
-    {
-        "conf": "ICSE 2021",
-        "url": "https://conf.researchr.org/track/icse-2021/icse-2021-papers",
-    },
-    {"conf": "FSE 2021", "url": "https://2021.esec-fse.org/track/fse-2021-papers"},
-    {
-        "conf": "ASE 2021",
-        "url": "https://conf.researchr.org/track/ase-2021/ase-2021-papers",
-    },
-    {
-        "conf": "ISSTA 2021",
-        "url": "https://conf.researchr.org/track/issta-2021/issta-2021-technical-papers",
-    },
-    # 2022
-    {
-        "conf": "ICSE 2022",
-        "url": "https://conf.researchr.org/track/icse-2022/icse-2022-papers",
-    },
-    {
-        "conf": "FSE 2022",
-        "url": "https://2022.esec-fse.org/track/fse-2022-research-papers",
-    },
-    {
-        "conf": "ASE 2022",
-        "url": "https://conf.researchr.org/track/ase-2022/ase-2022-research-papers",
-    },
-    {
-        "conf": "ISSTA 2022",
-        "url": "https://conf.researchr.org/track/issta-2022/issta-2022-technical-papers",
-    },
-    # 2023
-    {
-        "conf": "ICSE 2023",
-        "url": "https://conf.researchr.org/track/icse-2023/icse-2023-technical-track",
-    },
-    {
-        "conf": "FSE 2023",
-        "url": "https://2023.esec-fse.org/track/fse-2023-research-papers",
-    },
-    {
-        "conf": "ASE 2023",
-        "url": "https://conf.researchr.org/track/ase-2023/ase-2023-papers",
-    },
-    {
-        "conf": "ISSTA 2023",
-        "url": "https://2023.issta.org/track/issta-2023-technical-papers",
-    },
     # 2024
     {
         "conf": "ICSE 2024",
@@ -70,7 +8,10 @@ paper_list = [
         "conf": "FSE 2024",
         "url": "https://2024.esec-fse.org/track/fse-2024-research-papers",
     },
-    {"conf": "ISSTA 2024", "url": "https://2024.issta.org/track/issta-2024-papers"},
+    {
+        "conf": "ISSTA 2024", 
+        "url": "https://2024.issta.org/track/issta-2024-papers"
+    },
     {
         "conf": "ASE 2024",
         "url": "https://conf.researchr.org/track/ase-2024/ase-2024-research",
